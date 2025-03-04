2a654d7cc78b6c975b18914d184a336a284be0f3
Bleuh <bastiendouib@gmail.com>
2025-03-04 19:55:54 +0100

    Add docker + default articles CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [ArticlesModule],
 })
 export class AppModule {}
